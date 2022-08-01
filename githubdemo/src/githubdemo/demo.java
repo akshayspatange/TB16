e3004cf52c3758dc927279e50409fdb40e710f42
akshayspatange <akshaypatange1996@gmail.com>
2022-08-01 17:26:05 +0530

    By AkshayPatil

--- githubdemo/src/githubdemo/demo.java
+++ githubdemo/src/githubdemo/demo.java
@@ -2,10 +2,17 @@ package githubdemo;
 
 public class demo {
 
+	public void m1()
+	{
+		System.out.println("hii");	
+	}
 	public static void main(String[] args) {
+		
+	
 		// TODO Auto-generated method stub
 		
 		System.out.println("hellow");
+		
 
 	}
 
